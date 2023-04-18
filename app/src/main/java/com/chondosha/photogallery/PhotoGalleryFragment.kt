51a0a3d3fab800089e3eeb3107a2ea9a8868daf0
chondosha1234 <jmiller61193@gmail.com>
2023-04-18 16:19:25 -0400

    created photolistadapter and photoviewholder, and layout for list item. also moved fetch photos to viewmodel class

--- app/src/main/java/com/chondosha/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/chondosha/photogallery/PhotoGalleryFragment.kt
@@ -6,7 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.GridLayoutManager
 import com.chondosha.photogallery.databinding.FragmentPhotoGalleryBinding
 import kotlinx.coroutines.launch
@@ -20,6 +23,8 @@ class PhotoGalleryFragment : Fragment() {
         get() = checkNotNull(_binding) {
             "Cannot access binding because it is null. Is the view visibile?"
         }
+
+    private val photoGalleryViewModel: PhotoGalleryViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,8 +40,11 @@ class PhotoGalleryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         viewLifecycleOwner.lifecycleScope.launch {
-            val response = PhotoRepository().fetchPhotos()
-            Log.d(TAG, "Response received: $response")
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                photoGalleryViewModel.galleryItems.collect { items ->
+                    binding.photoGrid.adapter = PhotoListAdapter(items)
+                }
+            }
         }
     }
 
