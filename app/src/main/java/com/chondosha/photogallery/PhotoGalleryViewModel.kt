746f6d0272ac3b5a67806a6216391f4086c8a72e
chondosha1234 <jmiller61193@gmail.com>
2023-04-19 12:03:46 -0400

    added datastore and preferences repository and uiState data object so queries are saved between app restarts

--- app/src/main/java/com/chondosha/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/chondosha/photogallery/PhotoGalleryViewModel.kt
@@ -4,34 +4,42 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.chondosha.photogallery.api.GalleryItem
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 private const val TAG = "PhotoGalleryViewModel"
 
 class PhotoGalleryViewModel : ViewModel() {
     private val photoRepository = PhotoRepository()
+    private val preferencesRepository = PreferencesRepository.get()
 
-    private val _galleryItems: MutableStateFlow<List<GalleryItem>> = MutableStateFlow(emptyList())
-    val galleryItems: StateFlow<List<GalleryItem>>
-        get() = _galleryItems.asStateFlow()
+    private val _uiState: MutableStateFlow<PhotoGalleryUiState> = MutableStateFlow(
+        PhotoGalleryUiState()
+    )
+    val uiState: StateFlow<PhotoGalleryUiState>
+        get() = _uiState.asStateFlow()
 
     init {
         viewModelScope.launch {
-            try {
-                // val items = PhotoRepository().searchPhotos("cat")
-                val items = fetchGalleryItems("planets")
-                _galleryItems.value = items
-            } catch (ex: Exception) {
-                Log.e(TAG, "Failed to fetch gallery items", ex)
+            preferencesRepository.storedQuery.collectLatest { storedQuery ->
+                try {
+                    val items = fetchGalleryItems(storedQuery)
+
+                    _uiState.update { oldState ->
+                        oldState.copy(
+                            images = items,
+                            query = storedQuery
+                        )
+                    }
+                } catch (ex: Exception) {
+                    Log.e(TAG, "Failed to fetch gallery items", ex)
+                }
             }
         }
     }
 
     fun setQuery(query: String) {
-        viewModelScope.launch { _galleryItems.value = fetchGalleryItems(query) }
+        viewModelScope.launch { preferencesRepository.setStoredQuery(query) }
     }
 
     private suspend fun fetchGalleryItems(query: String): List<GalleryItem> {
@@ -42,3 +50,8 @@ class PhotoGalleryViewModel : ViewModel() {
         }
     }
 }
+
+data class PhotoGalleryUiState(
+    val images: List<GalleryItem> = listOf(),
+    val query: String = "",
+)
